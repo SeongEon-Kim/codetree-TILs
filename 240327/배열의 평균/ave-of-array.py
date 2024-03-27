2d8b5859deff01d083a8dbc81735591563fa674f
E ON <ykiki5778@inha.edu>
2024-03-27 10:34:08 +0900

    Title: 배열의 평균, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240327/배열의 평균/ave-of-array.py
+++ 240327/배열의 평균/ave-of-array.py
@@ -6,8 +6,11 @@ for idx in arr_2d:
     print(sum(idx)/len(idx), end=" ")
 print()
 
-for idx in range(len(arr_2d[0])):
-    print((arr_2d[0][idx] + arr_2d[1][idx])/2, end=" ")
+for idx_1d in range(len(arr_2d[0])):
+    sum_length = 0
+    for idx_2d in range(len(arr_2d)):
+        sum_length += arr_2d[idx_2d][idx_1d]
+    print(sum_length/len(arr_2d), end=" ")
 print()
 
 for idx in arr_2d:

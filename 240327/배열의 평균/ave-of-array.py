8a4689a5e614111a1b622338a49fd9eff0e96f42
E ON <ykiki5778@inha.edu>
2024-03-27 10:35:24 +0900

    Title: 배열의 평균, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/배열의 평균/ave-of-array.py
+++ 240327/배열의 평균/ave-of-array.py
@@ -3,14 +3,14 @@ arr_2d = [list(map(int, input().split())) for idx in range(n)]
 all_sum = 0 
 
 for idx in arr_2d:
-    print(sum(idx)/len(idx), end=" ")
+    print(round(sum(idx)/len(idx),1), end=" ")
 print()
 
 for idx_1d in range(len(arr_2d[0])):
     sum_length = 0
     for idx_2d in range(len(arr_2d)):
         sum_length += arr_2d[idx_2d][idx_1d]
-    print(sum_length/len(arr_2d), end=" ")
+    print(round(sum_length/len(arr_2d),1), end=" ")
 print()
 
 for idx in arr_2d:
@@ -18,4 +18,4 @@ for idx in arr_2d:
 
 len_arr_2d = len(arr_2d) * len(arr_2d[0])
 all_mean = all_sum/len_arr_2d
-print(all_mean)
+print(round(all_mean,1))

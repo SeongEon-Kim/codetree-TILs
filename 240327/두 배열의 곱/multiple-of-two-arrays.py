c28a98dc29250ca746798a070178d5b472d6ae7d
E ON <ykiki5778@inha.edu>
2024-03-27 11:01:26 +0900

    Title: 두 배열의 곱, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/두 배열의 곱/multiple-of-two-arrays.py
+++ 240327/두 배열의 곱/multiple-of-two-arrays.py
@@ -2,8 +2,7 @@ first_2d = [ list(map(int,input().split())) for _ in range(3)]
 space = list(map(int,input().split())) # 풀이 이후, 다른 방법 찾기
 second_2d = [ list(map(int,input().split())) for _ in range(3)]
 
-print(first_2d, second_2d)
-for i range(3):
-    for j range(3):
+for i in range(3):
+    for j in range(3):
         print(first_2d[i][j]*second_2d[i][j], end = " ")
     print()

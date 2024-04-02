ba3d0c73b873c048edc6e1bfcb937ab10a4e0caf
E ON <ykiki5778@inha.edu>
2024-04-03 00:53:55 +0900

    Title: 숫자 차례로 출력하기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240403/숫자 차례로 출력하기/to-output-numerically.py
@@ -0,0 +1,16 @@
+def upper(n): # 7 6 5 4 3 2 1
+    if n == 0 :
+        return
+    upper(n-1) # 6 5 4 3 2 1 0
+    print(n, end = " ")
+
+def lower(n):
+    if n == 0:
+        return
+    print(n, end = " ")
+    lower(n-1)
+
+n = int(input())
+
+upper(n)
+lower(n)

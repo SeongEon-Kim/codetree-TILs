04890d288110adb235953e834e2871f7b11c0c63
E ON <ykiki5778@inha.edu>
2024-04-11 13:12:01 +0900

    Title: 연속되는 수 4, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240411/연속되는 수 4/continuous-number4.py
+++ 240411/연속되는 수 4/continuous-number4.py
@@ -3,14 +3,14 @@ n = int(input())
 _list = [int(input()) for _ in range(n)]
 
 ans = []
-continuity = 0
+continuity = 1
 
 for idx in range(n):
     if idx == 0 or _list[idx-1] < _list[idx]:
         continuity += 1
         ans.append(continuity)
     else:
-        continuity = 0
+        continuity = 1
         ans.append(continuity)
-
+print(ans)
 print(max(ans))

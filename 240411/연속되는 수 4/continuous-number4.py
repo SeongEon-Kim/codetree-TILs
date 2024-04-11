afc4148c70d2699ff4a96c5c0c4dd86650105aee
E ON <ykiki5778@inha.edu>
2024-04-11 13:09:50 +0900

    Title: 연속되는 수 4, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240411/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+_list = [int(input()) for _ in range(n)]
+
+ans = []
+continuity = 0
+
+for idx in range(n):
+    if idx == 0 or _list[idx-1] < _list[idx]:
+        continuity += 1
+        ans.append(continuity)
+    else:
+        continuity = 0
+        ans.append(continuity)
+
+print(max(continuity))

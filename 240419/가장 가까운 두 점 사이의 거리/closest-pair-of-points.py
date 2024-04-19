af60cfa4aec6a2f13495425f185ca0155ebf8551
E ON <ykiki5778@inha.edu>
2024-04-19 14:54:49 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240419/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
+++ 240419/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -8,5 +8,6 @@ min_distance = sys.maxsize
 for i in range(n):
     for j in range(i+1, n):
         distance = (_list[i][0] - _list[j][0])**2 + (_list[i][1] - _list[j][1])**2
-    min_distance = min(min_distance, distance)
+        min_distance = min(min_distance, distance)
+    
 print(min_distance)

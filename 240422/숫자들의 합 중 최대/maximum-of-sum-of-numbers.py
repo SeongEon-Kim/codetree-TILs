92e7fa1d358d6c4f0c3365268df08727b8d373a5
E ON <ykiki5778@inha.edu>
2024-04-22 23:45:53 +0900

    Title: 숫자들의 합 중 최대, Time: 124ms, Memory: 25MB, Status: Passed - CodeTree

--- 240422/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
+++ 240422/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
@@ -1,9 +1,11 @@
 x,y = tuple(map(int, input().split()))
 
-ans = []
+ans_list = []
 for i in range(x, y+1):
-    a = i % 10
-    b = i // 10
-    ans.append(a+b)
+    str_i = str(i)
+    ans = 0
+    for j in range(len(str_i)):
+        ans += int(str_i[j])
+    ans_list.append(ans)
 
-print(max(ans))
+print(max(ans_list))

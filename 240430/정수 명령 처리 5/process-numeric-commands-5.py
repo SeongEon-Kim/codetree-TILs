2632460a00dd9f2e247aaea001983c4a68476ed9
E ON <ykiki5778@inha.edu>
2024-04-30 13:29:46 +0900

    Title: 정수 명령 처리 5, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240430/정수 명령 처리 5/process-numeric-commands-5.py
@@ -0,0 +1,26 @@
+def push_back(value):
+    ans_list.append(value)
+
+def pop_back():
+    del ans_list[-1]
+
+def size():
+    print(len(ans_list))
+
+def get(value):
+    print(ans_list[value-1])
+
+n = int(input())
+_list = [list(map(str,input().split())) for _ in range(9)]
+
+ans_list = []
+
+for idx in range(n):
+    if _list[idx][0]=="push_back":
+        push_back(int(_list[idx][1]))
+    elif _list[idx][0]=="pop_back":
+        pop_back()
+    elif _list[idx][0]=="size":
+        size()
+    else:
+        get(int(_list[idx][1]))

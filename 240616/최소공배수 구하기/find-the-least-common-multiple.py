63b23dfebf7c75e109960fc15d10007a583fe111
E ON <ykiki5778@inha.edu>
2024-06-16 17:47:56 +0900

    Title: 최소공배수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240616/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240616/최소공배수 구하기/find-the-least-common-multiple.py
@@ -2,7 +2,7 @@ n, m =list(map(int, input().split()))
 
 def LCP(n, m):
     for idx in range(max(n,m), n*m, 1):
-        id n%idx==0 and m%idx==0:
+        id idx%n==0 and idx&m==0:
             return idx
 
 print(LCP(n, m))

b98898bb59502c34cc8bd910da913283e0361893
E ON <ykiki5778@inha.edu>
2024-06-16 17:16:53 +0900

    Title: 최대공약수 구하기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240616/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,10 @@
+n, m = list(map(int,input().split()))
+
+def max_conum(n, m):
+    max_num = max(n, m)
+    answer = 0
+    for idx in range(max_num, 1, -1):
+        if max_num%idx==0 and n%idx==0:
+            return idx
+    
+print(max_conum(n, m))

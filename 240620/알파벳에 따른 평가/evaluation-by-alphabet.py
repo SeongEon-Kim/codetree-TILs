bd916438ce59996bd64847d749f8fdb6af5c323a
E ON <ykiki5778@inha.edu>
2024-06-20 19:23:21 +0900

    Title: 알파벳에 따른 평가, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240620/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+string = str(input())
+
+if string == 'S':
+    print('Superior')
+elif string == 'A':
+    print("Excellent")
+elif string == 'B':
+    print("Good")
+elif string == "C":
+    print("Usually")
+elif string == "D":
+    print("Effort")
+else :
+    print("Failure")

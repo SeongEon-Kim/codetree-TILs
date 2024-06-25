efea1d2a0da0f4ec380158fdfced31e9ffb3184b
E ON <ykiki5778@inha.edu>
2024-06-26 01:07:01 +0900

    Title: 함수를 이용한 온전수 판별, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240626/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
+++ 240626/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -2,14 +2,15 @@ a, b =list(map(int, input().split()))
 
 def special_num(n):
     if n %2!=0:
-        list_n = list(n)
+        list_n = str(n)
         if int(list_n[-1]) != 5:
-            if n % 3 ==0 and n% 9 !=0:
+            if n % 3 !=0 and n% 9 ==0:
                 return 1
             
     return 0
 
 sum = 0
 for i in range(a, b +1):
-    #sum += special_num(i)
-    print(special_num(i))
+    sum += special_num(i)
+
+print(sum)

826c0b82aa9b11f822e955b38a7b45945e290b65
E ON <ykiki5778@inha.edu>
2024-06-26 01:10:38 +0900

    Title: 함수를 이용한 온전수 판별, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240626/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
+++ 240626/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -4,9 +4,7 @@ def special_num(n):
     if n %2!=0:
         list_n = str(n)
         if int(list_n[-1]) != 5:
-            if n % 3 !=0 and n% 9 ==0:
-                return 0
-            else: 
+            if n % 3 !=0 or n% 9 ==0:
                 return 1
             
     return 0

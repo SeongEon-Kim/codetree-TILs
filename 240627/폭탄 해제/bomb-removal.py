a81c4cfbff438d06f1ec1df0916efa0f69ebd096
E ON <ykiki5778@inha.edu>
2024-06-27 11:32:23 +0900

    Title: 폭탄 해제, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240627/폭탄 해제/bomb-removal.py
+++ 240627/폭탄 해제/bomb-removal.py
@@ -20,3 +20,22 @@ boom(code, color, second)
 boom.print_code()
 boom.print_color()
 boom.print_second()
+
+
+# # 클래스 선언
+# class Bomb:
+# 	def __init__(self, unlock_code, linear_color, time):
+# 		self.unlock_code = unlock_code
+# 		self.linear_color = linear_color
+# 		self.time = time
+
+# # 변수 선언 및 입력
+# u_code, l_color, time = tuple(input().split())
+
+# # 객체 생성
+# b = Bomb(u_code, l_color, int(time))
+
+# # 출력
+# print(f"code : {b.unlock_code}")
+# print(f"color : {b.linear_color}")
+# print(f"second : {b.time}")

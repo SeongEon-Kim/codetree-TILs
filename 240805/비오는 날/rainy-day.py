efcc0fb225c016cfebcf7f126a5449968ad82b24
E ON <ykiki5778@inha.edu>
2024-08-05 17:55:25 +0900

    Title: 비오는 날, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240805/비오는 날/rainy-day.py
+++ 240805/비오는 날/rainy-day.py
@@ -10,10 +10,12 @@ n = int(input())
 arr = [tuple(input().split()) for _ in range(n)]
 
 rain_info = [RainDay(date, day, weather) for date, day, weather in arr]
+# 비오는 날만 필터링하여 새로운 리스트 생성
+rain_days = [day for day in rain_info if day.weather == 'Rain']
 
 target_idx = 0
-for i, day in enumerate(rain_info):
-    if day.date < rain_info[target_idx].date and day.weather == 'Rain':
+for i, day in enumerate(rain_days):
+    if day.date < rain_days[target_idx].date:
         target_idx = i
 
-print(rain_info[target_idx].date, rain_info[target_idx].day, rain_info[target_idx].weather)
+print(rain_days[target_idx].date, rain_days[target_idx].day, rain_days[target_idx].weather)

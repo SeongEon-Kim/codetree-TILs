fe1127348cf9ff2756cd52779932b6c66c627747
E ON <ykiki5778@inha.edu>
2025-03-27 16:53:02 +0900

    Title: 정수 명령 처리, Time: 143ms, Memory: 23MB, Status: Passed - Codetree

--- 250327/정수 명령 처리/process-numeric-commands.py
+++ 250327/정수 명령 처리/process-numeric-commands.py
@@ -1,3 +1,17 @@
+N = int(input())
+command = []
+value = []
+
+for _ in range(N):
+    line = input().split()
+    command.append(line[0])
+    if line[0] == "push":
+        value.append(int(line[1]))
+    else:
+        value.append(0)
+
+# ['push', 'size', 'empty', 'pop', 'push', 'size'] [3, 0, 0, 0, 3, 0]
+# Please write your code here.
 class Stack:
     def __init__(self):
         self.items = []

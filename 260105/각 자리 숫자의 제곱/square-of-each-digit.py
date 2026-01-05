799ed36c3efa3d3c434a8d7272fdad29b1bfd354
E ON <ykiki5778@inha.edu>
2026-01-05 15:17:25 +0900

    Title: 각 자리 숫자의 제곱, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260105/각 자리 숫자의 제곱/square-of-each-digit.py
+++ 260105/각 자리 숫자의 제곱/square-of-each-digit.py
@@ -2,9 +2,9 @@ N = int(input())
 
 # Please write your code here.
 def each_sum_sqared(N):
-    if N == 1:
-        return 1
+    if N == 0:
+        return 0
     
-    return each_sum_sqared(N%10) + N **2
+    return each_sum_sqared(N // 10) + (N %10)**2
 
 print(each_sum_sqared(N))

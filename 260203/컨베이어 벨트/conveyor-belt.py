7b782c9f07fce68c86b60e254bb9c2e5d5a48854
E ON <ykiki5778@inha.edu>
2026-02-03 14:48:20 +0900

    Title: 컨베이어 벨트, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 260203/컨베이어 벨트/conveyor-belt.py
+++ 260203/컨베이어 벨트/conveyor-belt.py
@@ -4,13 +4,13 @@ down_belt = list(map(int, input().split()))
 belt = up_belt + down_belt
 # print(belt) # 1 2 3 / 6 5 1 -> [] 1 1 2 / 3 6 5
 
-temp = belt[2*n-1]
 # print(temp)
-
-for i in range(2*n-1,0,-1):
-    belt[i] = belt[i-1]
-belt[0] = temp 
-# print(belt)1 1 2 / 3 6 5
+for _ in range(t): # t초 만큼 이동
+    temp = belt[2*n-1]
+    for i in range(2*n-1,0,-1):
+        belt[i] = belt[i-1]
+    belt[0] = temp 
+    # print(belt)1 1 2 / 3 6 5
 
 for i in belt[:n]:
     print(i, end=" ")

a4c222503f03b592afbee09938f754b19025231f
E ON <ykiki5778@inha.edu>
2026-02-04 17:13:51 +0900

    Title: 삼각형 컨베이어 벨트, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260204/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -0,0 +1,19 @@
+n, t = map(int, input().split())
+
+l = list(map(int, input().split()))
+r = list(map(int, input().split()))
+d = list(map(int, input().split()))
+
+triangle_belt = l + r + d 
+# 1 2 4 5 9 3 6 5 1
+
+for _ in range(t): # t초 동안
+    temp = triangle_belt[-1] # 1
+    for i in range(3*n-1,0,-1): # 1칸씩 이동
+        triangle_belt[i] = triangle_belt[i-1]
+    triangle_belt[0] = temp
+    
+for j in range(0,3*n, 3):
+    for k in range(n):
+        print(triangle_belt[k+j], end=" ")
+    print()

95d324e694b3b24bc295b14d9a0b262e637e5c8e
E ON <ykiki5778@inha.edu>
2026-02-05 23:02:08 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 260205/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 260205/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -1,101 +1,62 @@
-# # 격자 크기 n, 시작 r, c
-# n, r, c = tuple(map(int, input().split()))
+# 변수 선언 및 입력
+n, curr_x, curr_y = tuple(map(int, input().split()))
+a = [[0] * (n + 1)]
+for _ in range(n):
+    a.append([0] + list(map(int, input().split())))
 
-# # n x n 주어짐
-# grid = [ list(map(int,input().split())) for _ in range(n)]
-# print(n, r, c, grid)
+# 방문하게 되는 숫자들을 담을 곳입니다.
+visited_nums = []
 
 
-# max_value = grid[r][c] # 초기 최댓값
-# find_max = 0
-# answer_list = []
+# 범위가 격자 안에 들어가는지 확인합니다.
+def in_range(x, y):
+    return 1 <= x and x <= n and 1 <= y and y <= n
 
-# def in_range(r, c): # 범위 체크
-#     return r < n and c < n
 
-# def move(r, c, max_value):
-#     drs, dcs = [-1, 1, 0, 0], [0, 0, -1, 1] # 상하좌우 우선 순위
+# 범위가 격자 안이고, 해당 위치의 값이 더 큰지 확인합니다.
+def can_go(x, y, curr_num):
+    return in_range(x, y) and a[x][y] > curr_num
+
+
+# 조건에 맞춰 움직여봅니다.
+# 움직였다면 true를 반환하고
+# 만약 움직일 수 있는 곳이 없었다면 false를 반환합니다.
+def simulate():
+    global curr_x, curr_y
     
-#     for dr, dc in zip(drs, dcs):
-#         move_r, move_c = r + dr, c + dc # 상 하 좌 우 순으로 이동
-        
-#         if grid[move_r][move_c] > max_value: # 기존 값(max) 보다 크다면
-#             max_value = grid[move_r][move_c] # 최댓값 변경
-#             r, c = r + dr, c + dc # 좌표 이동
-#             answer_list.append(r, c) # 이동 좌표 입력
-#             find_max = 1 # 우선 순위 적용 되면 바로 통과
-#             return answer_list # 이동한 위치 반환
-#     return r, c # 이동 안하면 현재 위치 반환
-
-# result = []
-# for i in range(n):
-#     for j in range(n):
-#         result.append(move(i, j, max_value))
-
-# print(result)
-
-
-# # 격자 크기 n, 시작 r, c
-# n, r, c = tuple(map(int, input().split()))
-
-# # n x n 주어짐
-# grid = [ list(map(int,input().split())) for _ in range(n)]
-# print(n, r, c, grid)
-
-
-# max_value = grid[r][c] # 초기 최댓값
-# find_max = 0
-# answer_list = []
-
-# def in_range(r, c): # 범위 체크
-#     return r < n and c < n
-
-# def move(r, c, max_value):
-#     drs, dcs = [-1, 1, 0, 0], [0, 0, -1, 1] # 상하좌우 우선 순위
+    # 코딩의 간결함을 위해 
+    # 문제 조건에 맞게 상하좌우 순서로
+    # 방향을 정의합니다.
+    dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
     
-#     for dr, dc in zip(drs, dcs):
-#         move_r, move_c = r + dr, c + dc # 상 하 좌 우 순으로 이동
+    # 각각의 방향에 대해 나아갈 수 있는 곳이 있는지 확인합니다.
+    for dx, dy in zip(dxs, dys):
+        next_x, next_y = curr_x + dx, curr_y + dy
         
-#         if grid[move_r][move_c] > max_value: # 기존 값(max) 보다 크다면
-#             max_value = grid[move_r][move_c] # 최댓값 변경
-#             r, c = r + dr, c + dc # 좌표 이동
-#             answer_list.append(r, c) # 이동 좌표 입력
-#             find_max = 1 # 우선 순위 적용 되면 바로 통과
-#             return answer_list # 이동한 위치 반환
-#     return r, c # 이동 안하면 현재 위치 반환
+        # 갈 수 있는 곳이라면
+        # 이동하고 true를 반환합니다.
+        if can_go(next_x, next_y, a[curr_x][curr_y]):
+            curr_x, curr_y = next_x, next_y
+            return True
+    
+    # 움직일 수 있는 곳이 없었다는 의미로
+    # false 값을 반환합니다.
+    return False
 
-# result = []
-# for i in range(n):
-#     for j in range(n):
-#         result.append(move(i, j, max_value))
 
-# print(result)
-
-n, r, c = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(n)]
-
-# r, c가 1부터 주어지는 경우(대부분) -> 0-index로 변환
-r -= 1
-c -= 1
-
-drs = [-1, 1, 0, 0]   # 상, 하, 좌, 우
-dcs = [0, 0, -1, 1]
-
-path = [grid[r][c]]   # 시작 칸 포함
-
+# 초기 위치에 적혀있는 값을 답에 넣어줍니다.
+visited_nums.append(a[curr_x][curr_y])
 while True:
-    cur = grid[r][c]
-    moved = False
-
-    for dr, dc in zip(drs, dcs):
-        nr, nc = r + dr, c + dc
-        if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] > cur:
-            r, c = nr, nc
-            path.append(grid[r][c])
-            moved = True
-            break
-
-    if not moved:
+    # 조건에 맞춰 움직여봅니다.
+    greater_number_exist = simulate()
+    
+    # 인접한 곳에 더 큰 숫자가 없다면 종료합니다.
+    if not greater_number_exist:
         break
+    
+    # 움직이고 난 후의 위치를 답에 넣어줍니다.
+    visited_nums.append(a[curr_x][curr_y])
 
-print(*path)
+# 출력:
+for num in visited_nums:
+    print(num, end=' ')

83a93bbe988fca0d9a3dd729e53a0127c9727156
E ON <ykiki5778@inha.edu>
2026-02-12 14:41:15 +0900

    Title: N개 중에 M개 뽑기, Time: 60ms, Memory: 18MB, Status: Passed - Codetree

--- 260212/N개 중에 M개 뽑기/n-choose-m.py
+++ 260212/N개 중에 M개 뽑기/n-choose-m.py
@@ -6,7 +6,6 @@ def print_answer():
     for i in answer_list:
         print(i, end=" ")
     print()
-    
 def selection_n_of_m(curr_num):
     if curr_num == m +1:
         print_answer()
@@ -24,3 +23,39 @@ def selection_n_of_m(curr_num):
 
 
 selection_n_of_m(1)
+
+
+'''
+# 변수 선언 및 입력
+
+n, m = tuple(map(int, input().split()))
+combination = []
+
+
+# 방문한 원소들을 출력해줍니다.
+def print_combination():
+    for elem in combination:
+        print(elem, end = " ")
+    
+    print()
+
+
+def find_combination(curr_num, cnt):
+    # n개의 숫자를 모두 탐색했으면 더 이상 탐색하지 않습니다.
+    if curr_num == n + 1:
+        # 탐색하는 과정에서 m개의 숫자를 뽑은 경우 답을 출력해줍니다.
+        if cnt == m:
+            print_combination()
+        return
+
+    # curr_num에 해당하는 숫자를 사용했을 때의 경우를 탐색합니다.
+    combination.append(curr_num)
+    find_combination(curr_num + 1, cnt + 1)
+    combination.pop()
+
+    # curr_num에 해당하는 숫자를 사용하지 않았을 때의 경우를 탐색합니다.
+    find_combination(curr_num + 1, cnt)
+
+
+find_combination(1, 0)
+'''

259854355414829f686dd7c8089e01a0d32b6555
E ON <ykiki5778@inha.edu>
2026-02-19 11:23:16 +0900

    Title: 그래프 탐색, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260219/그래프 탐색/graph-traversal.py
+++ 260219/그래프 탐색/graph-traversal.py
@@ -1,52 +1,70 @@
-# n개의 정점, m개의 간선, 양방향
+# Sol 1) 인접리스트를 활용한 DFS
+# 변수 선언 및 입력
 n, m = tuple(map(int, input().split()))
 
+# index를 1번 부터 사용하기 위해 n+1만큼 할당
 graph = [[] for _ in range(n+1)]
+
 visited = [False for _ in range(n+1)]
 vertex_cnt = 0
 
-for i in range(m):
-    x, y = tuple(map(int, input().split()))
-    graph[x].append(y)
-    graph[y].append(x)
-
 def dfs(vertex):
     global vertex_cnt
-    for curr_vertex in graph[vertex]:
-        if not visited[curr_vertex]:
-            vertex_cnt += 1
-            visited[curr_vertex] = True
-            dfs(curr_vertex)
+    # 해당 정점에서 이어져있는 모든 정점을 탐색
+    for curr_v in graph[vertex]:
+        # 아직 간선이 존재하고 방문한 적 없는 정점에 대해서만 탐색을 진행
+        if not visited[vertex]:
+            visited[curr_v] = True
+            vertex_cnt +=1
+            dfs(curr_v)
+            
+for i in range(m):
+    v1, v2 = tuple(map(int, input().split()))
+
+    # 각 정점이 서로 이동이 가능한 양방향 그래프이기 때문에
+    # 각 정점에 대한 간선을 각각 저장해줍니다.
+    graph[v1].append(v2)
+    graph[v2].append(v1)
 
 visited[1] = True
 dfs(1)
+
 print(vertex_cnt)
+print(graph)
 
+# Sol 2) 인접행렬을 활용한 DFS
+'''
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
 
-# n, m = tuple(map(int, input().split()))
+# index를 1번 부터 사용하기 위해 n+1만큼 할당
+graph = [
+    [0 for _ in range(n+1)]
+    for _ in range(n+1)
+]
 
-# graph = [[] for _ in range(n+1)]
+visited = [False for _ in range(n+1)]
+vertex_cnt = 0
 
-# visited = [False for _ in range(n+1)]
-# vertex_cnt = 0
+def dfs(vertex):
+    global vertex_cnt
 
+    # 해당 정점에 이어져있는 모든 정점을 탐색해줍니다.
+    for curr_v in range(1, n+1):
+        # 아직 간선이 존재하고 방문한 적이 없는 정점에 대해서만 탐색을 진행
+        if graph[vertex][curr_v] and not visited[curr_v]:
+            visited[curr_v] = True
+            vertex_cnt += 1
+            dfs(curr_v)
 
-# def dfs(vertex):
-#     global vertex_cnt
-    
-#     for curr_v in graph[vertex]:
-#         if not visited[curr_v]:
-#             visited[curr_v] = True
-#             vertex_cnt +=1
-#             dfs(curr_v)
+for i in range(m):
+    v1, v2 = tuple(map(int, input().split()))
+    graph[v1][v2] = 1
+    graph[v2][v1] = 1
 
-# for i in range(m):
-#     v1, v2 = tuple(map(int, input().split()))
+visited[1] = True
+dfs(1)
 
-#     graph[v1].append(v2)
-#     graph[v2].append(v1)
-
-# visited[1] = True
-# dfs(1)
-
-# print(vertex_cnt)
+print(vertex_cnt)
+print(graph)
+'''

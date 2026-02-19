0b664149ed77de23fad5afa274e11a6906300cb4
E ON <ykiki5778@inha.edu>
2026-02-19 13:58:15 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 58ms, Memory: 17MB, Status: Passed - Codetree

--- 260219/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 260219/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,34 +1,37 @@
 # 그래프 탐색의 특성
 # 정점과 연결된 그래프 전체를 탐색할 수 있다. (-> 좌측 상단 출발에서 특정 지점 도달 가능성 탐색)
 # 아, 격자를 그래프 형태로 변환해야겠다!
-n, m = tuple(map(int, input().split()))
-
+n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 visited = [[False] * m for _ in range(n)]
 
 dxs, dys = [0, 1], [1, 0]
-x, y = 0, 0
 escape_status = False
 
 def in_range(x, y):
-    return 0<= x < n and 0<= y < m # x, y는 index(0, 0)
+    return 0 <= x < n and 0 <= y < m
 
 def dfs(x, y):
     global escape_status
-    for dx, dy in zip(dxs, dys):
-        nx = x + dx
-        ny = y + dy
-        if in_range(nx, ny) and grid[nx][ny] != 0 and visited[nx][ny] == False: 
-            # 범위 내 & 뱀 x & 방문 x
-            visited[nx][ny] = True # 방문
-            if nx == n-1 and ny == n-1:
-                escape_status = True
-            dfs(nx, ny)
-    
-visited[x][y] = True # 방문
-dfs(x, y) 
+    if escape_status:
+        return
 
-if escape_status:
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and grid[nx][ny] != 0 and not visited[nx][ny]:
+            visited[nx][ny] = True
+            if nx == n - 1 and ny == m - 1:
+                escape_status = True
+                return
+            dfs(nx, ny)
+
+# 시작이 막혔으면 실패
+if grid[0][0] == 0:
+    print(0)
+# 시작이 곧 도착(1x1)이면 시작칸이 1일 때 성공
+elif n == 1 and m == 1:
     print(1)
 else:
-    print(0)
+    visited[0][0] = True
+    dfs(0, 0)
+    print(1 if escape_status else 0)

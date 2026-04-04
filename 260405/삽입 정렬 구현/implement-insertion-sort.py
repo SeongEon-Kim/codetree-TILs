2498170dbbb7df4ed44c8055668e490dc503100c
E ON <ykiki5778@inha.edu>
2026-04-05 01:51:24 +0900

    Title: 삽입 정렬 구현, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260405/삽입 정렬 구현/implement-insertion-sort.py
+++ 260405/삽입 정렬 구현/implement-insertion-sort.py
@@ -6,7 +6,7 @@ def insertion_sort():
     for i in range(1, n):
         j = i - 1
         key = arr[i]
-        while j >= and arr[j] > key:
+        while j >= 0 and arr[j] > key:
             arr[j+1] = arr[j]
             j = j - 1
         arr[j+1] = key

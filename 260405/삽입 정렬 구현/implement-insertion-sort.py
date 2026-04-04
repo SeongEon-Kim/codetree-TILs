86f458cb195e4ecdcec486abe31a1f78f8536eb5
E ON <ykiki5778@inha.edu>
2026-04-05 01:50:42 +0900

    Title: 삽입 정렬 구현, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260405/삽입 정렬 구현/implement-insertion-sort.py
@@ -0,0 +1,17 @@
+# 변수 선언 및 입력
+n = int(input())
+arr = list(map(int, input().split()))
+
+def insertion_sort():
+    for i in range(1, n):
+        j = i - 1
+        key = arr[i]
+        while j >= and arr[j] > key:
+            arr[j+1] = arr[j]
+            j = j - 1
+        arr[j+1] = key
+
+insertion_sort()
+
+for elem in arr:
+    print(elem, end=" ")
